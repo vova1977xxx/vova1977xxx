3e0c9f4d1bae5654a7021fa94d885de9ac9675fa
gemivas <root@localhost>
2026-01-19 23:33:38 +0100

    deploy: copy web assets on update

--- update.sh
+++ update.sh
@@ -3,6 +3,8 @@ set -e
 cd /srv/gemivas-platform
 echo "[GEMIVAS] update"
 
+cp -a /srv/gemivas-platform/web/frontend/public/* /srv/web/frontend/public/ 2>/dev/null || true
+
 git add -A || true
 git commit -m "update" >/dev/null 2>&1 || true
 
