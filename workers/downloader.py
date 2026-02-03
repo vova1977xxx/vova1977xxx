f27e8e1dd42baf13ae2310bce65aa21378fac8d1
gemivas <root@localhost>
2026-02-03 12:01:53 +0100

    Rollback downloader to safe stub (pipeline stabilization)

--- workers/downloader.py
+++ workers/downloader.py
@@ -1,16 +1,3 @@
-import sys; sys.path.append("/srv/gemivas-platform")
-import os, time, json
-from fsm.api import safe_set
-FSM_DIR='/srv/memory/fsm/items'
-def run():
-    while True:
-        for f in os.listdir(FSM_DIR):
-            p=os.path.join(FSM_DIR,f)
-            try:
-                d=json.load(open(p))
-                if d.get('state')=='scouted':
-                    safe_set(f.replace('.json',''),'scouted','downloaded',{})
-            except: pass
-        time.sleep(5)
-
-if __name__=='__main__': run()
+import time
+while True:
+    time.sleep(30)
